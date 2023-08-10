7b0b127ac6be3650130f2584b06d282850e7845a
joyce <joyce.c.odiaka@gmail.com>
2023-08-10 15:16:22 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,20 +1,18 @@
 /*
  * File: puzzle.c
- * Author: Joyce Odiaka
- * Date: 8th August 2023
- * Description: Prints "Programming is like building a multilingual puzzle,"
+ * Description: Prints the message "Programming is like building a multilingual puzzle,"
  *              followed by a new line, using the puts function.
  */
 
 #include <stdio.h>
 
 /**
- * main - Prints "Programming is like building a multilingual puzzle,
- *              followed by a newline"
+ * main - Prints "Programming is like building a multilingual puzzle,followed by a newline."
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    puts("Programming is like building a multilingual puzzle,");
-    return (0);
+puts("Programming is like building a multilingual puzzle,");
+return (0);
 }
