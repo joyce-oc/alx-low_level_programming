221c0ae62292826542da46beddedae2437791066
joyce <joyce.c.odiaka@gmail.com>
2023-08-17 17:49:24 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - Prints numbers from 0 to 9 followed by a new line
+ */
+void print_numbers(void)
+{
+    int number;
+
+    for (number = 0; number <= 9; number++)
+    {
+        _putchar(number + '0');
+    }
+
+    _putchar('\n');
+}
