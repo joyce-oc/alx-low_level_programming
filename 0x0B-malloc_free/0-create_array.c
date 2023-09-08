c6ba11b69058c385d9fccdb24d22ad5d03294603
joyce <joyce.c.odiaka@gmail.com>
2023-09-08 07:50:45 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,25 +9,19 @@
  * Return: If size is 0 or if memory allocation fails, returns NULL.
  * Otherwise, returns a pointer to the newly created array.
  */
-
 char *create_array(unsigned int size, char c)
 {
-    char *array;
+char *array;
+if (size == 0)
+return (NULL);
 
-    if (size == 0)
-        return (NULL);
-
-    array = (char *)malloc(sizeof(char) * size);
-
-    if (array == NULL)
-        return (NULL);
-
-    {
-        unsigned int i;
-
-        for (i = 0; i < size; i++)
-            array[i] = c;
-    }
-
-    return (array);
+array = (char *)malloc(sizeof(char) * size);
+if (array == NULL)
+return (NULL);
+{
+unsigned int i;
+for (i = 0; i < size; i++)
+array[i] = c;
+}
+return (array);
 }
