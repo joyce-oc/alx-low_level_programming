99f41eeb9f40681e894855be15afb7593784e2d2
joyce <joyce.c.odiaka@gmail.com>
2023-09-08 08:08:39 -0700

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -10,23 +10,22 @@
  */
 char *_strdup(char *str)
 {
-    char *duplicate;
-    unsigned int length = 0, i;
+char *duplicate;
+unsigned int length = 0, i;
 
-    if (str == NULL)
-        return (NULL);
+if (str == NULL)
+return (NULL);
 
-    while (str[length])
-        length++;
+while (str[length])
+length++;
+duplicate = malloc(sizeof(char) * (length + 1));
 
-    duplicate = malloc(sizeof(char) * (length + 1));
+if (duplicate == NULL)
+return (NULL);
 
-    if (duplicate == NULL)
-        return (NULL);
+for (i = 0; i <= length; i++)
+duplicate[i] = str[i];
 
-    for (i = 0; i <= length; i++)
-        duplicate[i] = str[i];
-
-    return (duplicate);
+return (duplicate);
 }
 
