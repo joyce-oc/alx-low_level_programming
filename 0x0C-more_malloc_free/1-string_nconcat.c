e31a4369ca71ac1fd62debc0773f9994da82dc84
joyce <joyce.c.odiaka@gmail.com>
2023-09-08 21:51:40 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,35 +12,35 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-    unsigned int len1 = 0, len2 = 0, i, j;
-    char *concatenated;
+unsigned int len1 = 0, len2 = 0, i, j;
+char *concatenated;
 
-    if (s1 == NULL)
-        s1 = "";
-    if (s2 == NULL)
-        s2 = "";
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
 
-    while (s1[len1])
-        len1++;
-    while (s2[len2])
-        len2++;
+while (s1[len1])
+len1++;
+while (s2[len2])
+len2++;
 
-    if (n >= len2)
-        n = len2;
+if (n >= len2)
+n = len2;
 
-    concatenated = malloc(sizeof(char) * (len1 + n + 1));
+concatenated = malloc(sizeof(char) * (len1 + n + 1));
 
-    if (concatenated == NULL)
-        return (NULL);
+if (concatenated == NULL)
+return (NULL);
 
-    for (i = 0; i < len1; i++)
-        concatenated[i] = s1[i];
+for (i = 0; i < len1; i++)
+concatenated[i] = s1[i];
 
-    for (j = 0; j < n; j++)
-        concatenated[i + j] = s2[j];
+for (j = 0; j < n; j++)
+concatenated[i + j] = s2[j];
 
-    concatenated[i + j] = '\0';
+concatenated[i + j] = '\0';
 
-    return (concatenated);
+return (concatenated);
 }
 
