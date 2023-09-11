733c9d350ef9e2275122989869d0aa595f68985b
joyce <joyce.c.odiaka@gmail.com>
2023-09-11 05:48:52 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ * @argc: The number of command-line arguments
+ * @argv: An array of command-line argument strings
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+    if (argc > 0)
+    {
+        printf("%s\n", argv[0]);
+        return 0;
+    }
+    return 1;
+}
