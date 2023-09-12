4258160806340bef617817528926eef2c368ded9
joyce <joyce.c.odiaka@gmail.com>
2023-09-12 00:49:20 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,7 +14,9 @@ struct dog
     char *owner;
 };
 
-struct dog *create_dog(char *name, float age, char *owner);
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 void free_dog(struct dog *d);
 
