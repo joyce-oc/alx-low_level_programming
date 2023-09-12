8b394a979dacb40d513309306c0e9aecc66b9165
joyce <joyce.c.odiaka@gmail.com>
2023-09-12 00:39:20 -0700

    2-print_dog.c

---
+++ 0x0E-structures_typedef/1-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "dog.h"
+
+int main(void)
+{
+    struct dog my_dog;
+
+    init_dog(&my_dog, "Poppy", 3.5, "Bob");
+
+    print_dog(&my_dog);
+
+    return (0);
+}
---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * print_dog - Prints information about a dog
+ * @d: Pointer to the struct dog to print
+ */
+void print_dog(struct dog *d)
+{
+    if (d != NULL)
+    {
+        printf("Name: %s\n", (d->name != NULL) ? d->name : "(nil)");
+        printf("Age: %.2f\n", d->age);
+        printf("Owner: %s\n", (d->owner != NULL) ? d->owner : "(nil)");
+    }
+}
