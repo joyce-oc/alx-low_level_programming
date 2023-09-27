ad41427b40f73eb0ef77073e16073a203215ce2d
joyce-oc <joyce.c.odiaka@gmail.com>
2023-09-27 10:45:24 -0700

    4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,24 +1,17 @@
 #include "main.h"
 
 /**
- * _pow_recursion - Calculate the value of x raised to the power of y.
- * @x: The base value.
- * @y: The exponent value.
+ * _pow_recursion - returns the value of x raised to the power of y
+ * @x: value to raise
+ * @y: power
  *
- * Return: The result of x^y, or -1 for an error if y is negative.
+ * Return: result of the power
  */
 int _pow_recursion(int x, int y)
 {
-    if (y < 0)
-    {
-        return -1; // Return -1 for error if y is negative
-    }
-    else if (y == 0)
-    {
-        return 1; // Any number raised to the power of 0 is 1
-    }
-    else
-    {
-        return x * _pow_recursion(x, y - 1); // Recursive case: x^y = x * x^(y-1)
-    }
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
 }
