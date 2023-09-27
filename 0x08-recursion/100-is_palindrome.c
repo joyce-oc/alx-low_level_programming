21a18b567d1ce1910dcf6a1b76578a4824009bca
joyce-oc <joyce.c.odiaka@gmail.com>
2023-09-27 10:58:54 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,35 +1,47 @@
 #include "main.h"
 
-/**
- * _palindrome_helper - Helper function to check if a string is a palindrome.
- * @s: The string to check.
- * @start: The starting index of the current comparison.
- * @end: The ending index of the current comparison.
- *
- * Return: 1 if the string is a palindrome, 0 otherwise.
- */
-int _palindrome_helper(char *s, int start, int end)
-{
-    if (start >= end)
-    {
-        return 1; // Base case: entire string checked, it's a palindrome
-    }
-    if (s[start] != s[end])
-    {
-        return 0; // Characters don't match, not a palindrome
-    }
-    
-    return _palindrome_helper(s, start + 1, end - 1); // Check next characters
-}
+int check_pal(char *s, int i, int len);
+int _strlen_recursion(char *s);
 
 /**
- * is_palindrome - Check if a string is a palindrome.
- * @s: The string to check.
+ * is_palindrome - checks if a string is a palindrome
+ * @s: string to reverse
  *
- * Return: 1 if the string is a palindrome, 0 otherwise.
+ * Return: 1 if it is, 0 it's not
  */
 int is_palindrome(char *s)
 {
-    int len = _strlen_recursion(s);
-    return _palindrome_helper(s, 0, len - 1);
+	if (*s == 0)
+		return (1);
+	return (check_pal(s, 0, _strlen_recursion(s)));
+}
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: string to calculate the length of
+ *
+ * Return: length of the string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
+}
+
+/**
+ * check_pal - checks the characters recursively for palindrome
+ * @s: string to check
+ * @i: iterator
+ * @len: length of the string
+ *
+ * Return: 1 if palindrome, 0 if not
+ */
+int check_pal(char *s, int i, int len)
+{
+	if (*(s + i) != *(s + len - 1))
+		return (0);
+	if (i >= len)
+		return (1);
+	return (check_pal(s, i + 1, len - 1));
 }
